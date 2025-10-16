4d552a89d53bb91bd1adab194c5205e319ff422c
arthurhintz <arthurrhintz04@gmail.com>
2025-10-16 19:48:31 -0300

    paralelelismo

--- simulacao_2d_mxarma.R
+++ simulacao_2d_mxarma.R
@@ -3,7 +3,11 @@
 source("simu_2d_mxarma.R")
 source("fit_2d_mxarma.R")
 
+library(doParallel)
+library(doRNG)
 
+        
+        
 # Ver os valores do artigo rarma2d
 phi_values <- c(0.4, 0.3, 0.1, 0)
 phi <- matrix(phi_values, ncol = 2, nrow = 2, byrow = T)
@@ -35,14 +39,32 @@ mf <- matrix(NA, ncol = length(vp), nrow = nrep)
 #colnames(results) <- c("parâmetros", "par_estimados", "viés_relativo", "EQM")
 colnames(mf) <- c("alpha", paste0("phi", 1:p1), paste0("theta", 1:q1))
 
+
+# paralelismo
+
+# Infraestrutura paralela
+n_cores <- parallel::detectCores() - 2L
+cl <- parallel::makeCluster(n_cores)
+on.exit(parallel::stopCluster(cl), add = TRUE)
+
+doParallel::registerDoParallel(cl)
+# reprodutibilidade independente do número de núcleos
+doRNG::registerDoRNG(1248)   
+
+parallel::clusterExport(cl, c("mxarma2d.fit", "mxarma2d.sim"), 
+                        envir = environment())
+
+
 for (j in seq_along(nvalores)) {
   
   k <- kvalores[j]
   n <- nvalores[j]
   
+  start_time <- Sys.time()
+  
   #if (n < 100) nrep <- 4000 else nrep <- 2000
   
-  for (i in 1:nrep) {
+foreach::foreach(i = 1:nrep, .combine = rbind) %dopar% {
     
     rasu <- mxarma2d.sim(n = n, k = k, alpha = alpha,
                          phi = phi, theta = theta)
@@ -52,13 +74,22 @@ for (j in seq_along(nvalores)) {
     fit <- mxarma2d.fit(y = y, p = p, q = q)
     mf[i,] <- fit$coeff
     
-    cat("n = ", j, "  ",  "nrep =", i)
+    end_time <- Sys.time()
+    elapsed <- round(difftime(end_time, start_time, units = "secs"), 2)
+    
+    cat(
+      sprintf("[%s] n = %d (%d×%d) | rep = %d | tempo = %s s\n",
+              format(Sys.time(), "%H:%M:%S"),
+              j, n, k, i, elapsed)
+    )
+    
   }
   
-  
-  write.table(mf, paste0("simu_2DMxARMA_nk", n,".txt"))
-  
+  write.table(mf, paste0("simu_2DMxARMA_nk", n, ".txt"),
+              row.names = FALSE, quote = FALSE)
 }
+
+cat("Simulação concluída às", format(Sys.time(), "%H:%M:%S"), "\n")
 
 
 
